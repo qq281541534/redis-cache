afd24fcd8d817f3a8906eb1c5d602d9d82e4c027
liuyu <281541534@qq.com>
2015-04-26 23:30:02 +0800

    添加了对缓存的注释

--- src/main/java/com/liuyu/rediscache/service/UserService.java
+++ src/main/java/com/liuyu/rediscache/service/UserService.java
@@ -12,6 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.liuyu.rediscache.dao.UserDao;
 import com.liuyu.rediscache.entity.User;
 
+/**
+ * 参数选项：
+ *	 	@Cacheable：根据方法的请求参数对其结果进行缓存，即第一次从数据库查询真实的数据
+ * 	@CachePut：根据方法的请求参数对其结果进行缓存；和Cacheable不同的是，每次都会触发真实的方法调用 
+ * 	@CachEvict:根据方法的请求参数对缓存进行清空
+ * @author liuyu
+ *
+ */
 @Service("userService")
 @Transactional
 public class UserService {
@@ -19,12 +27,21 @@ public class UserService {
 	@Autowired
 	private UserDao userDao;
 
+	/**
+	 * 添加用户时，直接将key对应的数据存入到缓存
+	 * @param user
+	 */
 //	@CacheEvict(value = "user", key = "'all'")
 	@CachePut(value = "user", key = "#user.id")
 	public void addUser(User user) {
 		userDao.save(user);
 	}
 
+	/**
+	 * 如果缓存中有key对应的数据，则从缓存中取；如果没有则查询数据库
+	 * @param id
+	 * @return
+	 */
 	@Cacheable(value = "user", key = "#id")
 	public User findById(String id) {
 		return userDao.findById(id, User.class);
@@ -35,12 +52,21 @@ public class UserService {
 		return userDao.list("from User");
 	}
 
+	/**
+	 * 修改用户时，直接将key对应的数据更新到缓存
+	 * @param user
+	 */
 //	@CacheEvict(value = "user", key="'all'")
 	@CachePut(value = "user", key = "#user.id")
 	public void updateUser(User user) {
 		userDao.update(user);
 	}
 
+	/**
+	 * 删除用户时，清空key对应的缓存
+	 * @param id
+	 * @return
+	 */
 //	@CacheEvict(value = "user", key = "'all'")
 	@CacheEvict(value = "user", key = "#id")
 	public String deleteUser(String id) {
